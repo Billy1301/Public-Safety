7f85915a6d91a49edd59e5a295c165f2d5d42d01
adao1123 <adao1193@gmail.com>
2016-07-16 10:30:25 -0700

    Implemented the PPT to widget

--- JustInCase/app/src/main/java/billy/justincase/WidgetProvider.java
+++ JustInCase/app/src/main/java/billy/justincase/WidgetProvider.java
@@ -30,17 +30,40 @@ public class WidgetProvider extends AppWidgetProvider {
             intent.setData(Uri.parse(url));
             Log.i("", "onUpdate: ");
 
+            /*
+            Call police intent
+             */
             String policeNum = "6504400404"; //temp number
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:"+policeNum));
 
+            /*
+            Call Contacts Intent
+             */
+            String EventType = "109,112";
+            // Call Type for 1-1 PTT call is 0.
+            String CallType = "0,0";
+            String ParaType = "20,0";
+            String commandString = "";
+            String MDN = "2647950721";
+            commandString = "ET="+ EventType + ";CT="+CallType+ ";PT="+ParaType+";UR="+ MDN +";";
+            // Send Broadcast here..
+            Intent contactIntent = new Intent();
+            // Broadcast action
+            intent.setAction("com.kodiak.intent.action.mobileapi");
+            // Data - formatted command string
+            intent.putExtra("PTTData",commandString);
+//        sendBroadcast(intent);
+
+
             PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
             PendingIntent callPolicePending = PendingIntent.getActivity(context,0,callIntent,0);
+            PendingIntent callContactPending = PendingIntent.getBroadcast(context,0,contactIntent,0);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
 
             views.setOnClickPendingIntent(R.id.call_police_button, callPolicePending);
-            views.setOnClickPendingIntent(R.id.call_friend_button, pending);
+            views.setOnClickPendingIntent(R.id.call_friend_button, callContactPending);
             views.setOnClickPendingIntent(R.id.text_contacts_button, pending);
             views.setOnClickPendingIntent(R.id.record_button, pending);
 //            views.setOnClickPendingIntent(R.id.button2, );
@@ -48,5 +71,24 @@ public class WidgetProvider extends AppWidgetProvider {
             Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
         }
 
+
     }
+
+    private void invokeLocationAndAudio(String MDN)
+    {
+        String EventType = "109,112";
+        // Call Type for 1-1 PTT call is 0.
+        String CallType = "0,0";
+        String ParaType = "20,0";
+        String commandString = "";
+        commandString = "ET="+ EventType + ";CT="+CallType+ ";PT="+ParaType+";UR="+ MDN +";";
+        // Send Broadcast here..
+        Intent intent = new Intent();
+        // Broadcast action
+        intent.setAction("com.kodiak.intent.action.mobileapi");
+        // Data - formatted command string
+        intent.putExtra("PTTData",commandString);
+//        sendBroadcast(intent);
+    }
+
 }
